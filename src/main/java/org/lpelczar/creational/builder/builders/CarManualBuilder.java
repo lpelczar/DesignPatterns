9f50635f285ef36d19fe7bd4afbc9429257a4e9f
lpelczar <lukepelczar@gmail.com>
2020-10-14 18:51:00 +0200

    Add car manual builder

---
+++ src/main/java/org/lpelczar/creational/builder/builders/CarManualBuilder.java
@@ -0,0 +1,51 @@
+package org.lpelczar.creational.builder.builders;
+
+import org.lpelczar.creational.builder.cars.Manual;
+import org.lpelczar.creational.builder.cars.Type;
+import org.lpelczar.creational.builder.components.Engine;
+import org.lpelczar.creational.builder.components.GPSNavigator;
+import org.lpelczar.creational.builder.components.Transmission;
+import org.lpelczar.creational.builder.components.TripComputer;
+
+public class CarManualBuilder implements Builder {
+    private Type type;
+    private int seats;
+    private Engine engine;
+    private Transmission transmission;
+    private TripComputer tripComputer;
+    private GPSNavigator gpsNavigator;
+
+    @Override
+    public void setType(Type type) {
+        this.type = type;
+    }
+
+    @Override
+    public void setSeats(int seats) {
+        this.seats = seats;
+    }
+
+    @Override
+    public void setEngine(Engine engine) {
+        this.engine = engine;
+    }
+
+    @Override
+    public void setTransmission(Transmission transmission) {
+        this.transmission = transmission;
+    }
+
+    @Override
+    public void setTripComputer(TripComputer tripComputer) {
+        this.tripComputer = tripComputer;
+    }
+
+    @Override
+    public void setGPSNavigator(GPSNavigator gpsNavigator) {
+        this.gpsNavigator = gpsNavigator;
+    }
+
+    public Manual getResult() {
+        return new Manual(type, seats, engine, transmission, tripComputer, gpsNavigator);
+    }
+}
